6c3b7b050beee9aadded5e557de875d2ced849ce
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-09-10 13:50:14 +1000

    Only deploy master branch

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,9 @@ set -o pipefail
 # Print shell input lines as they are read.
 set -v
 
+# The git branch we are on
+readonly GITBRANCH="$(git symbolic-ref --short -q HEAD)"
+
 # login to cloud foundry if env vars are present
 login() {
 
@@ -27,10 +30,23 @@ login() {
 main() {
   login
   cd site
+
   # while iterating, its faster to use cf push so we get buildpack caching
   # if uptime becomes more important, change to zero-downtime-push
-  cf push original-ausgov -f manifest.yml
   #cf zero-downtime-push original-ausgov -f manifest.yml
+
+  case "${GITBRANCH}" in
+    master)
+      cf push original-ausgov -f manifest.yml
+      ;;
+
+    *)
+      # uncomment this if you want to push this branch as a separate app
+      # Remember to delete the app on cloud.gov.au when you are done
+      # cf push original-ausgov-`basename \"${GITBRANCH}\"` -f manifest.yml
+      ;;
+  esac
+
 }
 
 main $@
