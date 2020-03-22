52764f252b4fcfd7bb643dedfa693a3a953fee41
sukhrajghuman <sukhrajghuman@live.com>
2020-03-23 09:02:39 +1100

    Add 'updated' text

--- meta-date.sh
+++ meta-date.sh
@@ -1,7 +1,11 @@
+# Generate meta tag for last updated date
 CURRENT_DATE=$(TZ="Australia/Sydney" date +'%Y-%m-%dT%H:%M:%S%z')
-DATE_TEXT=$(TZ="Australia/Sydney" date +'%l:%M%p %Z %A, %d %B %Y')
 META_TAG="<meta property="\"og:updated_time\"" content="\"$CURRENT_DATE\"" />"
-DATE_TAG="<p>$DATE_TEXT<p>"
+
+# Generate HTML tag for last updated date
+DATE_TEXT=$(TZ="Australia/Sydney" date +'%l:%M%p %Z %A, %d %B %Y')
+DATE_TAG="<p>Updated:$DATE_TEXT<p>"
+
 sed -i '' "s@<!--TIME_META-->@$META_TAG@" site/public/index.html
 sed -i '' "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" site/public/index.html
 
