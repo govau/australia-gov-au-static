23c21f64d0f9592b38da0f767a84d32df0f308ae
sukhrajghuman <sukhrajghuman@live.com>
2020-03-22 13:23:55 +1100

    add script to update html last updated tag

--- meta-date.sh
+++ meta-date.sh
@@ -1,4 +1,8 @@
 CURRENT_DATE=$(TZ="Australia/Sydney" date +'%Y-%m-%dT%H:%M:%S%z')
+DATE_TEXT=$(TZ="Australia/Sydney" date +'%l:%M%p %Z %A, %d %B %Y')
 META_TAG="<meta property="\"og:updated_time\"" content="\"$CURRENT_DATE\"" />"
-sed -i '' "s@<!--TIME-META-->@$META_TAG@" site/public/index.html
+DATE_TAG="<p>$DATE_TEXT<p>"
+sed -i '' "s@<!--TIME_META-->@$META_TAG@" site/public/index.html
+sed -i '' "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" site/public/index.html
+
 
