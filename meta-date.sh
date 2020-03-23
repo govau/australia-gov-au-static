75dedb231e235f80dd51cf77344fa0fb48aeaac3
sukhrajghuman <sukhrajghuman@live.com>
2020-03-24 10:28:56 +1100

    content_update

--- meta-date.sh
+++ meta-date.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 
-# Get commit message of last change to index.html and translate to lowercase
-COMMIT_MESSAGE=$(git log -1 --format="%s" site/public/index.html | tr '[:upper:]' '[:lower:]')
+# Path to landing page
+INDEX_PAGE="site/public/index.html"
 
-# Check commit message to see if it was a content update. If not, do nothing
-if [[ $COMMIT_MESSAGE == content_update* ]];
-then
+# Get last content commit date in iso8601 format
+CONTENT_COMMIT_DATE=$(TZ=australia/sydney git log --all -i --grep='content_update' --date=iso --format="%aI" $INDEX_PAGE | head -n1)
 
-  # Generate datetime of last commit to index.html in iso8601 format
-  CURRENT_DATE=$(git log -1 --date=iso --format="%aI" site/public/index.html)
+# Get last content commit date in readable format
+DATE_TEXT=$(TZ=australia/sydney git log --all -i --grep='content_update' --date=format-local:"%l:%M%p %Z %A, %d %B %Y" --format="%ad" $INDEX_PAGE | head -n1)
 
-  # Generate meta tag to be inserted into HTML page
-  META_TAG="<meta property="\"og:updated_time\"" content="\"$CURRENT_DATE\"" />"
+# Generate meta tag to be inserted into HTML page
+META_TAG="<meta property="\"og:updated_time\"" content="\"$CONTENT_COMMIT_DATE\"" />"
 
+# Generate HTML tag
+DATE_TAG="<p>Updated: $DATE_TEXT<p>"
 
-  # Generate datetime of last commit to the index.html page
-  DATE_TEXT=$(git log -1 --date=format:"%l:%M%p %Z %A, %d %B %Y" --format="%ad" site/public/index.html)
-
-  # Generate HTML tag
-  DATE_TAG="<p>Updated: $DATE_TEXT<p>"
-
-  sed -i '' "s@<!--TIME_META-->@$META_TAG@" site/public/index.html
-  sed -i '' "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" site/public/index.html
-
-else
-  echo "Content has not been changed, time data update not required."
-fi
-
+sed -i '' "s@<!--TIME_META-->@$META_TAG@" $INDEX_PAGE
+sed -i '' "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" $INDEX_PAGE
