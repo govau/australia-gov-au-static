2e6c99a122f6b47eccfd7c5b1328af70e229b979
sukhrajghuman <sukhrajghuman@live.com>
2020-03-24 12:46:04 +1100

    add TZ environment variable

--- meta-date.sh
+++ meta-date.sh
@@ -2,12 +2,11 @@
 
 # Path to landing page
 INDEX_PAGE="site/public/index.html"
-
 # Get last content commit date in iso8601 format
-CONTENT_COMMIT_DATE=$(TZ=australia/sydney git log --all -i --grep='content_update' --date=iso --format="%aI" $INDEX_PAGE | head -n1)
+CONTENT_COMMIT_DATE=$(git log --all -i --grep='content_update' --date=iso --format="%aI" $INDEX_PAGE | head -n1)
 
 # Get last content commit date in readable format
-DATE_TEXT=$(TZ=australia/sydney git log --all -i --grep='content_update' --date=format-local:"%l:%M%p %Z %A, %d %B %Y" --format="%ad" $INDEX_PAGE | head -n1)
+DATE_TEXT=$(git log --all -i --grep='content_update' --date=format-local:"%l:%M%p %Z %A, %d %B %Y" --format="%ad" $INDEX_PAGE | head -n1)
 
 # Generate meta tag to be inserted into HTML page
 META_TAG="<meta property="\"og:updated_time\"" content="\"$CONTENT_COMMIT_DATE\"" />"
@@ -15,5 +14,7 @@ META_TAG="<meta property="\"og:updated_time\"" content="\"$CONTENT_COMMIT_DATE\"
 # Generate HTML tag
 DATE_TAG="<p>Updated: $DATE_TEXT<p>"
 
-sed -i "s@<!--TIME_META-->@$META_TAG@" $INDEX_PAGE
-sed -i "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" $INDEX_PAGE
+# sed -i "s@<!--TIME_META-->@$META_TAG@" $INDEX_PAGE
+# sed -i "s@<!--LAST_UPDATED_TAG-->@$DATE_TAG@" $INDEX_PAGE
+
+echo "Last update time: $DATE_TEXT"
